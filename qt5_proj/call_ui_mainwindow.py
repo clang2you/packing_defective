ef2057d49ad7fb73932cd08a87a1ced630f2058e
Lex Chen <clang2you@outlook.com>
2020-02-28 17:30:33 +0800

    first_test

---
+++ qt5_proj/call_ui_mainwindow.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from Ui_packaging_recovery_rate import Ui_MainWindow
+
+class MyMainForm(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyMainForm, self).__init__(parent)
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mywin = MyMainForm()
+    mywin.show()
+    sys.exit(app.exec_())
