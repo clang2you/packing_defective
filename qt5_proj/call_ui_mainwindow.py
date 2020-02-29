de4898f73c65ebd7a5ef9ad593abab5440be8606
Lex Chen <clang2you@outlook.com>
2020-02-29 17:48:46 +0800

    add pyqtcharts

--- qt5_proj/call_ui_mainwindow.py
+++ qt5_proj/call_ui_mainwindow.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtChart import QBarSet,QBarCategoryAxis,QBarSeries,QChart,QChartView
 from mainForm import Ui_MainWindow
 
 
@@ -8,7 +9,27 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyMainForm, self).__init__(parent)
         self.setupUi(self)
+    
+    def create_bar_chart(self):
+        set0 = QBarSet('Jane')
+        set1 = QBarSet('John')
+        series = QBarSeries()
+        axis = QBarCategoryAxis()
+        chart = QChart()
+        
+        categories = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
 
+        set0.append([1, 2, 3, 4, 5, 6])
+        set1.append([5, 0, 0, 4, 0, 7])
+        series.append(set0)
+        series.append(set1)
+        chart.addSeries(series)
+        axis.append(categories)
+
+        chart.setTitle('Simple barchart example')
+        chart.createDefaultAxes()
+        chart.setAxisX(axis, series)
+        return chart
 
 def GetSystemDefinitionToChangeFontSizeAndLabelSize(form):
     # 获取系统分辨率更改 label 尺寸及字体大小
@@ -22,7 +43,6 @@ def GetSystemDefinitionToChangeFontSizeAndLabelSize(form):
     height = QApplication.desktop().screenGeometry().height()
     # 高度小于 800，字体及高度都修改为20
     if height <= 800:
-        form.frame.setMaximumSize(QtCore.QSize(230, 520))
         form.frame.setMinimumSize(QtCore.QSize(230, 520))
         font = QtGui.QFont()
         font.setFamily("微软雅黑")
@@ -45,10 +65,20 @@ def GetSystemDefinitionToChangeFontSizeAndLabelSize(form):
             label.setFont(font)
             label.setMinimumSize(QtCore.QSize(16777215, 20))
 
+# def AddQtChartsIntoMainForm(form):
+#     # 增加图表项目到主窗口
+#     # main code here
+
+#     # example code here
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mywin = MyMainForm()
     GetSystemDefinitionToChangeFontSizeAndLabelSize(mywin)
+    charview = QChartView(mywin.create_bar_chart(), mywin.frame_2)
+    print(str(mywin.tab_3.height()) + " width:" + str(mywin.tab_3.width()))
+    charview.setGeometry(0,0, mywin.tab_3.width(),mywin.tab_3.height())
     mywin.show()
     sys.exit(app.exec_())
