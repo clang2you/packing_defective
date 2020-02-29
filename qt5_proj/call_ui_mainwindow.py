f474c6a3120f4877e5e4f7b972d3d917c6898ac7
Lex Chen <clang2you@outlook.com>
2020-02-29 15:58:20 +0800

    change to using findChild function

--- qt5_proj/call_ui_mainwindow.py
+++ qt5_proj/call_ui_mainwindow.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import QtGui,QtCore
+from PyQt5 import QtGui, QtCore, QtWidgets
 from mainForm import Ui_MainWindow
 
 
@@ -11,12 +11,16 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
 
 
 def GetSystemDefinitionToChangeFontSizeAndLabelSize(form):
+    # 获取系统分辨率更改 label 尺寸及字体大小
+    # 先设定各个 label 字体不同颜色
     form.label_2.setStyleSheet("QLabel{color: red}")
     form.label_4.setStyleSheet("QLabel{color:blue}")
     form.label_6.setStyleSheet("QLabel{color:pink}")
     form.label_9.setStyleSheet("QLabel{color:green}")
     form.label_7.setStyleSheet("QLabel{color:purple}")
+    # 从系统分辨率中提取桌面高度
     height = QApplication.desktop().screenGeometry().height()
+    # 高度小于 800，字体及高度都修改为20
     if height <= 800:
         form.frame.setMaximumSize(QtCore.QSize(230, 520))
         form.frame.setMinimumSize(QtCore.QSize(230, 520))
@@ -25,30 +29,21 @@ def GetSystemDefinitionToChangeFontSizeAndLabelSize(form):
         font.setPointSize(20)
         font.setBold(True)
         font.setWeight(50)
-        form.label_2.setFont(font)
-        form.label_2.setMinimumSize(QtCore.QSize(16777215, 20))
-        form.label_4.setFont(font)
-        form.label_4.setMinimumSize(QtCore.QSize(16777215, 20))
-        form.label_6.setFont(font)
-        form.label_6.setMinimumSize(QtCore.QSize(16777215, 20))
-        form.label_9.setFont(font)
-        form.label_9.setMinimumSize(QtCore.QSize(16777215, 20))
-        form.label_7.setFont(font)
-        form.label_7.setMinimumSize(QtCore.QSize(16777215, 20))
+        labels = ('label_2', 'label_4', 'label_6', 'label_7', 'label_9')
+        for label_name in labels:
+            label = form.frame.findChild((QtWidgets.QLabel), label_name)
+            label.setFont(font)
+            label.setMinimumSize(QtCore.QSize(16777215, 20))
         font = QtGui.QFont()
         font.setFamily("微软雅黑 Light")
         font.setPointSize(20)
         font.setBold(False)
         font.setWeight(50)
-        form.label.setFont(font)
-        form.label.setMinimumSize(QtCore.QSize(16777215, 20))
-        form.label_3.setFont(font)
-        form.label_3.setMinimumSize(QtCore.QSize(16777215, 20))
-        form.label_5.setFont(font)
-        form.label_5.setMinimumSize(QtCore.QSize(16777215, 20))
-        form.label_8.setFont(font)
-        form.label_8.setMinimumSize(QtCore.QSize(16777215, 20))
-
+        labels = ('label', 'label_3', 'label_5', 'label_8')
+        for label_name in labels:
+            label = form.frame.findChild((QtWidgets.QLabel), label_name)
+            label.setFont(font)
+            label.setMinimumSize(QtCore.QSize(16777215, 20))
 
 
 if __name__ == "__main__":
